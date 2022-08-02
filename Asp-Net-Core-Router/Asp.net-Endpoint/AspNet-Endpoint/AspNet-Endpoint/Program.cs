9d4c588def93987c80e7937e9618fd3d7f804652
youli <www.xingjijiandui@aliyun.com>
2022-08-02 23:30:50 +0800

    rabbimq

---
+++ Asp-Net-Core-Router/Asp.net-Endpoint/AspNet-Endpoint/AspNet-Endpoint/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseRouting();
+app.MapGet("/", () => "Hello World!");
+
+
+//app.UseEndpoints()
+app.Run();
---
+++ RabbitMQ/Receive/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// 1.实例化连接工厂
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+//2. 建立连接
+using (var connection = factory.CreateConnection())
+{
+    //3. 创建信道
+    using (var channel = connection.CreateModel())
+    {
+        //4. 申明队列
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        //5. 构造消费者实例
+        var consumer = new EventingBasicConsumer(channel);
+        //6. 绑定消息接收后的事件委托
+        consumer.Received += (model, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            Console.WriteLine(" [x] Received {0}", message);
+            Thread.Sleep(6000); //模拟耗时
+            Console.WriteLine(" [x] Done");
+        };
+
+        //7. 启动消费者
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
+
