3ce862757e33a260434b986616130caa7076c7b7
youli <www.xingjijiandui@aliyun.com>
2022-08-28 00:25:22 +0800

    TaskCompletionSource

---
+++ .Net-Task/TaskCompletionSource/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+var waitForStop = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+Console.WriteLine("start");
+Task.Run(async () =>
+{
+    await Task.Delay(5000);
+    waitForStop.TrySetResult(null);
+    Console.WriteLine("set value");
+});
+
+Console.WriteLine("wait");
+await waitForStop.Task;
+Console.WriteLine("complete");
+
+
