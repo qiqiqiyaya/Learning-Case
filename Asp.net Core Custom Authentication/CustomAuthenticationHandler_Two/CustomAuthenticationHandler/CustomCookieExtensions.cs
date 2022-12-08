28aa1ded5d794c5133c3f4ced8d8ecdb60cf20c6
youli <www.xingjijiandui@aliyun.com>
2022-12-08 22:49:31 +0800

    CustomAuthenticationHandler_Two

---
+++ Asp.net Core Custom Authentication/CustomAuthenticationHandler_Two/CustomAuthenticationHandler/CustomCookieExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace CustomAuthenticationHandler
+{
+    public static class CustomCookieExtensions
+    {
+        /// <summary>
+        /// Adds cookie authentication to <see cref="AuthenticationBuilder"/> using the specified scheme.
+        /// <para>
+        /// Cookie authentication uses a HTTP cookie persisted in the client to perform authentication.
+        /// </para>
+        /// </summary>
+        /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+        /// <param name="authenticationScheme">The authentication scheme.</param>
+        /// <param name="displayName">A display name for the authentication handler.</param>
+        /// <param name="configureOptions">A delegate to configure <see cref="CookieAuthenticationOptions"/>.</param>
+        /// <returns>A reference to <paramref name="builder"/> after the operation has completed.</returns>
+        public static AuthenticationBuilder AddCustomCookie(this AuthenticationBuilder builder, string authenticationScheme, string? displayName, Action<CookieAuthenticationOptions> configureOptions)
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<CookieAuthenticationOptions>, PostConfigureCookieAuthenticationOptions>());
+            builder.Services.AddOptions<CookieAuthenticationOptions>(authenticationScheme).Validate(o => o.Cookie.Expiration == null, "Cookie.Expiration is ignored, use ExpireTimeSpan instead.");
+            return builder.AddScheme<CookieAuthenticationOptions, CustomCookieAuthenticationHandler>(authenticationScheme, displayName, configureOptions);
+        }
+    }
+}
