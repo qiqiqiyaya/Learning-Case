53f39e916dc2b49c29f5bbc6ffd593c51d07e539
youli <www.xingjijiandui@aliyun.com>
2023-01-01 17:09:53 +0800

    System.Runtime.CompilerServices

---
+++ System.Runtime.CompilerServices/.Net 6 Caller/Caller/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+TestFuc();
+ArgumentNotNull(new TestResult(){AAA = "fdsafsd"});
+Console.ReadKey();
+
+
+
+void TestFuc([CallerMemberName] string memberName = default, [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = default)
+{
+    Console.WriteLine("memberName " + memberName);
+    Console.WriteLine("filePath " + filePath);
+    Console.WriteLine("lineNumber " + lineNumber);
+}
+
+void ArgumentNotNull(TestResult argument, [CallerArgumentExpression("argument")] string argumentExpression = default)
+{
+    Console.WriteLine("argumentExpression " + argument);
+}
+
+
+public class TestResult
+{
+    public string AAA { get; set; }
+}
---
+++ System.Runtime.CompilerServices/Net4.7Caller/Net4.7Caller/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net4._7Caller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestFuc();
+            Console.ReadKey();
+
+        }
+
+        static void TestFuc([CallerMemberName] string memberName = default, [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = default)
+        {
+            Console.WriteLine("memberName " + memberName);
+            Console.WriteLine("filePath " + filePath);
+            Console.WriteLine("lineNumber " + lineNumber);
+        }
+
+
+        public class TestResult
+        {
+            public string AAA { get; set; }
+        }
+    }
+}
+
+
